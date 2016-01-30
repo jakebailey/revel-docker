27b281083de8d4b2a5545495c1b5b9f29cb62215
Jake Bailey <jake@jakebailey.io>
2016-01-29 20:57:27 -0600

    initial commit, pretty straightforward

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# revel-docker
+revel-docker
+============
+
 Docker integration for Revel
+
+Usage
+-----
+
+Add the `docker.endpoint` configuration option to `app.conf`. This uses the standard docker socket.
+
+```
+docker.endpoint = unix:///var/run/docker.sock
+```
+
+Then, ask revel to init the docker client on app start:
+
+```go
+revel.OnAppStart(reveldocker.Init)
+```
+
+Embed into your controller:
+
+```go
+type App struct {
+	*revel.Controller
+	reveldocker.DockerController
+}
+```
+
+And add the intercept:
+
+```go
+revel.InterceptMethod((*reveldocker.DockerController).Before, revel.BEFORE)
+```
---
+++ docker.go
@@ -0,0 +1,34 @@
+package reveldocker
+
+import (
+	"github.com/fsouza/go-dockerclient"
+	"github.com/revel/revel"
+)
+
+var client *docker.Client
+
+// Init initializes the docker client.
+func Init() {
+	endpoint, ok := revel.Config.String("docker.endpoint")
+	if !ok {
+		revel.ERROR.Fatal("No docker endpoint specified.")
+	}
+
+	c, err := docker.NewClient(endpoint)
+	if err != nil {
+		revel.ERROR.Fatal(err)
+	}
+
+	client = c
+}
+
+// DockerController allows embedding a docker client into a revel controller.
+type DockerController struct {
+	DockerClient *docker.Client
+}
+
+// Before adds the docker client to the controller.
+func (c *DockerController) Before() revel.Result {
+	c.DockerClient = client
+	return nil
+}
